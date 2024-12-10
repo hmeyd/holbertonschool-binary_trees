7330e166123bfb53abbddfd3a5a2577fad728cd0
hmeyd <yyahmedsalm@gmail.com>
2024-12-10 12:39:57 +0100

    betty task 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -2,17 +2,17 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - creates a binary tree node
+ * binary_tree_is_root - creates a binary tree node
  * @node: le parent dans le node
  * Return: 0 ou 1
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-        if (node == NULL)
-                return (0);
-        binary_tree_is_root(node->parent);
-        if (node->parent == NULL)
-                return (1);
-        else
-                return (0);
+	if (node == NULL)
+		return (0);
+	binary_tree_is_root(node->parent);
+	if (node->parent == NULL)
+		return (1);
+	else
+		return (0);
 }
