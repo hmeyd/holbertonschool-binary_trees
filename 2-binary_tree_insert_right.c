a9651ec3f61e0c4647578f8d51b9ac6ac047a734
hmeyd <yyahmedsalm@gmail.com>
2024-12-10 01:28:54 +0100

    task 1 task 2

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - creates a binary tree node
+ * @parent: le parent dans le node
+ * @value: le vareur dans le new_noeud
+ * Return: le nouvau noeud
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->parent = parent;
+	if (parent->left != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
+	parent->right = new_node;
+	return (new_node);
+}
